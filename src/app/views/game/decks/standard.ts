3153e93dc89fe0acd7482b9a34ea71ce4ad495e5
Natan Zasępa <natan.z@kbsystems.pl>
2017-08-22 23:48:03 +0200

    Game reducer

---
+++ src/app/views/game/decks/standard.ts
@@ -0,0 +1,100 @@
+import { CardType } from '../game.models';
+
+export const standardDeck: CardType[] = [
+  {
+    name: 'A',
+    image: ''
+  },
+  {
+    name: 'B',
+    image: ''
+  },
+  {
+    name: 'C',
+    image: ''
+  },
+  {
+    name: 'D',
+    image: ''
+  },
+  {
+    name: 'E',
+    image: ''
+  },
+  {
+    name: 'F',
+    image: ''
+  },
+  {
+    name: 'G',
+    image: ''
+  },
+  {
+    name: 'H',
+    image: ''
+  },
+  {
+    name: 'I',
+    image: ''
+  },
+  {
+    name: 'J',
+    image: ''
+  },
+  {
+    name: 'K',
+    image: ''
+  },
+  {
+    name: 'L',
+    image: ''
+  },
+  {
+    name: 'M',
+    image: ''
+  },
+  {
+    name: 'N',
+    image: ''
+  },
+  {
+    name: 'O',
+    image: ''
+  },
+  {
+    name: 'P',
+    image: ''
+  },
+  {
+    name: 'R',
+    image: ''
+  },
+  {
+    name: 'S',
+    image: ''
+  },
+  {
+    name: 'T',
+    image: ''
+  },
+  {
+    name: 'U',
+    image: ''
+  },
+  {
+    name: 'W',
+    image: ''
+  },
+  {
+    name: 'X',
+    image: ''
+  },
+  {
+    name: 'Y',
+    image: ''
+  },
+  {
+    name: 'Z',
+    image: ''
+  }
+];
